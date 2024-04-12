7e8e151472a4cb119399a00a5500a374c1d6f979
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-04-12 16:37:14 +0300

    Changed language

--- CountryMKR/CountryMKR.py
+++ CountryMKR/CountryMKR.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 def test_population_change(file_name):
     data = pd.read_csv(file_name, header=None)
-    data.columns = ['Країна', 'Рік', 'Населення']
-    data = data.sort_values(['Країна', 'Рік'])
-    data['Зміна населення'] = data.groupby('Країна')['Населення'].diff().fillna(0)
+    data.columns = ['Country', 'Year', 'Population']
+    data = data.sort_values(['Country', 'Year'])
+    data['Population Change'] = data.groupby('Country')['Population'].diff().fillna(0)
     return data
 
 file_name = "population_data.txt"
 population_data = test_population_change(file_name)
 print(population_data)
-
