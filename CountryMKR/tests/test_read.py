a21ce8c95b8b66e848d08d8030cd6ad33aa2211e
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-04-12 16:34:11 +0300

    Added fixtures in tests

--- CountryMKR/tests/test_read.py
+++ CountryMKR/tests/test_read.py
@@ -3,6 +3,14 @@ import os
 import pandas as pd
 import pytest
 
+# Фікстура для створення тестових даних
+@pytest.fixture
+def create_file():
+    def _create_file(file_name, data):
+        data.to_csv(file_name, header=False, index=False)
+    return _create_file
+
+# Параметризація для виконання тестів з різними наборами даних
 @pytest.mark.parametrize('file_name, expected', [
     ('test_data1.txt', pd.DataFrame({
         'Країна': ['Україна', 'Україна'],
@@ -15,8 +23,11 @@ import pytest
         'Населення': [145912025, 145912987]
     })),
 ])
-def test_write(file_name, expected):
-    expected.to_csv(file_name, header=False, index=False)
+def test_write(create_file, file_name, expected):
+    create_file(file_name, expected)
+    data = pd.read_csv(file_name, header=None)
+    data.columns = ['Країна', 'Рік', 'Населення']
+    pd.testing.assert_frame_equal(data, expected)
 
 @pytest.mark.parametrize('file_name, expected', [
     ('test_data1.txt', pd.DataFrame({
@@ -28,13 +39,12 @@ def test_write(file_name, expected):
         'Зміна населення': [float(145912987 - 145912025)]
     })),
 ])
-
 def test_population_change(file_name, expected):
     data = pd.read_csv(file_name, header=None)
     data.columns = ['Країна', 'Рік', 'Населення']
     data = data.sort_values(['Країна', 'Рік'])
     data['Зміна населення'] = data.groupby('Країна')['Населення'].diff().fillna(0)
     data = data.groupby('Країна')['Зміна населення'].sum().reset_index()
-    print(data)
     pd.testing.assert_frame_equal(data, expected)
-
+    # Видалення файлу після виконання тесту
+    os.remove(file_name)
