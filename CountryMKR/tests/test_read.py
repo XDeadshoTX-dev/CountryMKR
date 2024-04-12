ea0a24003b45e69dd634c26951bf465f6b4fd20b
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-04-12 16:39:11 +0300

    Changed language

--- CountryMKR/tests/test_read.py
+++ CountryMKR/tests/test_read.py
@@ -3,48 +3,48 @@ import os
 import pandas as pd
 import pytest
 
-# Фікстура для створення тестових даних
+# Fixture for creating test data
 @pytest.fixture
 def create_file():
     def _create_file(file_name, data):
         data.to_csv(file_name, header=False, index=False)
     return _create_file
 
-# Параметризація для виконання тестів з різними наборами даних
+# Parametrization for running tests with different sets of data
 @pytest.mark.parametrize('file_name, expected', [
     ('test_data1.txt', pd.DataFrame({
-        'Країна': ['Україна', 'Україна'],
-        'Рік': [2020, 2021],
-        'Населення': [44134693, 43993638]
+        'Country': ['Ukraine', 'Ukraine'],
+        'Year': [2020, 2021],
+        'Population': [44134693, 43993638]
     })),
     ('test_data2.txt', pd.DataFrame({
-        'Країна': ['Франція', 'Франція'],
-        'Рік': [2020, 2021],
-        'Населення': [145912025, 145912987]
+        'Country': ['France', 'France'],
+        'Year': [2020, 2021],
+        'Population': [145912025, 145912987]
     })),
 ])
 def test_write(create_file, file_name, expected):
     create_file(file_name, expected)
     data = pd.read_csv(file_name, header=None)
-    data.columns = ['Країна', 'Рік', 'Населення']
+    data.columns = ['Country', 'Year', 'Population']
     pd.testing.assert_frame_equal(data, expected)
 
 @pytest.mark.parametrize('file_name, expected', [
     ('test_data1.txt', pd.DataFrame({
-        'Країна': ['Україна'],
-        'Зміна населення': [float(43993638 - 44134693)]
+        'Country': ['Ukraine'],
+        'Population Change': [float(43993638 - 44134693)]
     })),
     ('test_data2.txt', pd.DataFrame({
-        'Країна': ['Франція'],
-        'Зміна населення': [float(145912987 - 145912025)]
+        'Country': ['France'],
+        'Population Change': [float(145912987 - 145912025)]
     })),
 ])
 def test_population_change(file_name, expected):
     data = pd.read_csv(file_name, header=None)
-    data.columns = ['Країна', 'Рік', 'Населення']
-    data = data.sort_values(['Країна', 'Рік'])
-    data['Зміна населення'] = data.groupby('Країна')['Населення'].diff().fillna(0)
-    data = data.groupby('Країна')['Зміна населення'].sum().reset_index()
+    data.columns = ['Country', 'Year', 'Population']
+    data = data.sort_values(['Country', 'Year'])
+    data['Population Change'] = data.groupby('Country')['Population'].diff().fillna(0)
+    data = data.groupby('Country')['Population Change'].sum().reset_index()
     pd.testing.assert_frame_equal(data, expected)
-    # Видалення файлу після виконання тесту
+    # Deleting the file after the test is run
     os.remove(file_name)
